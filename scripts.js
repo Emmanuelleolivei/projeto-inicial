e16faf9d5a473db674ba928a66cc5e09a0b2c0ab
Emmanuelleolivei <emmanuelleoliveira878@gmail.com>
2024-03-20 10:01:28 -0300

    formulario

--- scripts.js
+++ scripts.js
@@ -2,6 +2,47 @@ var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
 var nome = document.getElementById("nome");
 
+var email = document.getElementById("email");
+
+var telefone = document.getElementById("telefone");
+
+var cep = document.getElementById("cep");
+
+var logradouro = document.getElementById("logradouro");
+
+var numero = document.getElementById("numero");
+
+var complemento = document.getElementById("complemento");
+
+var bairro = document.getElementById("bairro");
+
+var cidade = document.getElementById("cidade");
+
+var estado = document.getElementById("estado");
+
+var saida = document.getElementById("saida-de-dados");
+
 function alertar (event){
-    alert("voce clicou no botao!!! " + " "+ nome.value);
+   // alert("voce clicou no botao!!! " + " "+ nome.value);
+   // var numero = 7;
+   // var resultado = numero % 2;
+   // if(resultado == 0){
+   // alert(este numero é par!");   
+   // }
+   saida.innerText = "nome: " + nome.value +
+       "\n email: " + email.value +
+       "\n telefone: " + telefone.value +
+       "\n CEP: " + cep.value +
+       "\n logradouro: " + logradouro.value +
+       "\n numero: " + numero.value +
+       "\n complemento: " + complemento.value +
+       "\n bairro: " + bairro.value +
+       "\n cidade: " + cidade.value +
+       "\n estado: " + estado.value; 
+       
+       
+       
+
+       
+
 }
